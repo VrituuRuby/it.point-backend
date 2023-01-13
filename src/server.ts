cb25e8f24e02853c37801d006dca294721db9d6d
VictorVelozo <victor.velozo@outlook.com>
2023-01-13 15:19:34 -0300

    chore: add ts-node and nodemon setup

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(3333, () => console.log("Server running on port 3333"));
+app.listen(3333, () => console.log("Server running on port 3333!"));
 
 export { app };
